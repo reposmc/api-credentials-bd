f2411230bfa08fccd009b3630dd4d94439ef9bd2
Leonel López <lalopez@cultura.gob.sv>
2023-04-09 16:37:54 -0600

    Delete innecessary comments

--- src/controllers/credentials.controller.js
+++ src/controllers/credentials.controller.js
@@ -10,12 +10,6 @@ const server = require("fastify");
  * @returns
  */
 const getCredentials = async (req, res) => {
-  // const token = req.headers["x-access-token"];
-
-  // if (!libs.verifyToken(token)) {
-  //   return res.status(401).send({ msg: "Token not provided or invalid." });
-  // }
-
   const credentials = await Credential.find(req.body);
 
   return res.send({
@@ -32,13 +26,6 @@ const getCredentials = async (req, res) => {
  * @returns
  */
 const credentialsByFilter = async (req, res) => {
-  // const token = req.headers["x-access-token"];
-  // console.log(token);
-
-  // if (!libs.verifyToken(token)) {
-  //   return res.status(401).send({ msg: "Token not provided or invalid." });
-  // }
-
   const credentials = await Credential.find(req.body);
 
   return res.send({
